e2b660a7466037232ac7310d2cda40956e3cc470
gobsz <gabimmoreira14@gmail.com>
2025-11-14 21:07:46 -0300

    adding back route hash

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { SkillProvider } from "./context/skill-context";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
@@ -7,25 +7,23 @@ import { SimulatorPage } from "./pages/simulator";
 import { ResultsPage } from "./pages/results";
 import { AboutPage } from "./pages/about";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <SkillProvider>
-      <HashRouter>
-        <div className="flex flex-col min-h-screen bg-slate-900">
-          <Header />
+      <div className="flex flex-col min-h-screen bg-slate-900">
+        <Header />
 
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/simulator" element={<SimulatorPage />} />
-              <Route path="/results" element={<ResultsPage />} />
-              <Route path="/about" element={<AboutPage />} />
-            </Routes>
-          </main>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/simulator" element={<SimulatorPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
 
-          <Footer />
-        </div>
-      </HashRouter>
+        <Footer />
+      </div>
     </SkillProvider>
   );
 };
